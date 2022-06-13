4ceb901455a6ba81fa4e31292ab733a231d25fb1
LHC <leehc0427@naver.com>
2022-06-13 16:20:10 +0900

    Dollar change coin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState('');
+  const [submit, setSubmit] = useState('');
+  
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => {
+    setDollar(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setSubmit(dollar / Math.floor(coins[event.target[0].value].quotes.USD.price));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+     {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <form onSubmit={onSubmit}>
+            <select>
+              {coins.map((coin, index) => <option value={index} key={index}>{coin.name} - ({coin.symbol}) - ${coin.quotes.USD.price}</option>)}
+            </select>
+            <hr />
+            <input onChange={onChange} value={dollar} type="number"></input><button>Change coin</button>
+            <p>{submit}</p>
+          </form>
+          
+        )}
     </div>
-  );
+  )
 }
 
 export default App;
